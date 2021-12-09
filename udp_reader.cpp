3356b24a2cd9445d036ca2a103e1aedc1403f242
Gurnoor Singh <gurnoor6@gmail.com>
2021-12-09 13:17:21 +0000

    find process name for inode done

--- udp_reader.cpp
+++ udp_reader.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <sys/stat.h>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -8,6 +9,10 @@ void print(vector<T> &v) {
         cout << i << " ";
     }
     cout << endl;
+}
+
+bool isNumber(string str){
+    return all_of(str.begin(), str.end(), ::isdigit);
 }
 
 vector<string> getWords(string line){
@@ -26,6 +31,45 @@ int getPortFromString(string str){
     return port;
 }
 
+// get process name for given pid
+string getProcessName(int pid){
+    string path = "/proc/" + to_string(pid) + "/comm";
+    ifstream file(path);
+    string line;
+    getline(file, line);
+    return line;
+}
+
+int getPidForInode(int inode){
+    const string dirname = "/proc";
+    vector<string> process_dirs;
+    for(const auto &entry: fs::directory_iterator(dirname)){
+        string path = entry.path().string();
+        if(isNumber(path.substr(path.find_last_of("/")+1))){
+            process_dirs.push_back(path);
+        }
+    }
+
+    for(string dir: process_dirs){
+        for(const auto &entry: fs::directory_iterator(dir + "/fd")){
+            string path = entry.path().string();
+            struct stat file_stats;
+            int ret = stat(path.c_str(), &file_stats);
+            if(ret < 0){
+                cerr << "system call stat failed" << endl;
+            }
+            else{
+                int inode_num = file_stats.st_ino;
+                if(inode_num == inode){
+                    return stoi(dir.substr(dir.find_last_of("/")+1));
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
 void readfile(string filename){
     string line;
     ifstream file(filename);
@@ -42,17 +86,15 @@ void readfile(string filename){
         int dest_port = getPortFromString(words[2]);
         if(dest_port != 53) continue;
         int inode = stoi(words[9]); // inode is at 9th position
+        cout << "src_port: " << src_port << " dest_port: " << dest_port << " inode: " << inode << endl;
+        int pid = getPidForInode(inode);
+        cout << "process: " << getProcessName(pid) << endl;
     }
     file.close();
 }
 
-void getFilesInDirectory(string dirname){
-    for (const auto &entry: fs::directory_iterator(dirname))
-        cout << entry.path() << endl;
-}
-
 int main(){
-    // while(true)
-    //     readfile("/proc/net/udp");
-    getFilesInDirectory("/proc");
+    while(true){
+        readfile("/proc/net/udp");
+    }
 }
